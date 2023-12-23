eff0d78287bb4f7c85e09dcbc8cd447a808f1a42
ayush-verma018 <vayush1806@gmail.com>
2023-12-24 03:05:12 +0530

    Controllers for CRUD Operations

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,6 @@
 import User from "../model/userModal.js";
 
+//controller for creating new data and storing in the DB
 export const create = async (req, res) => {
   try {
     const userData = new User(req.body);
@@ -10,6 +11,67 @@ export const create = async (req, res) => {
 
     const savedData = await userData.save();
     res.status(200).json(savedData);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+//controller to get all data from DB and provide as a response
+export const getAll = async (req, res) => {
+  try {
+    const userData = await User.find();
+    if (!userData) {
+      return res.status(404).json({ msg: "No Data Found" });
+    }
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+//controller to get one data of user with given id and provide as a response
+export const getOne = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ msg: "User not Found" });
+    }
+    res.status(200).json(userExist);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+//controller to update the data of a user with given ID
+export const update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(401).json({ msg: "User not Found" });
+    }
+
+    const updatedData = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedData);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+//controller to delete the user data based on ID
+export const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(401).json({ msg: "User not Found" });
+    }
+
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ msg: "User Deleted Successfully" });
   } catch (error) {
     res.status(500).json({ error: error });
   }
