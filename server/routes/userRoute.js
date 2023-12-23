c5cbfb5c38d01b33c42fbfde1a92acece5dc58dd
ayush-verma018 <vayush1806@gmail.com>
2023-12-24 03:06:39 +0530

    Routes for server calls

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { create } from "../controller/userController.js";
+import {
+  create,
+  deleteUser,
+  getAll,
+  getOne,
+  update,
+} from "../controller/userController.js";
 
 const route = express.Router();
 
+//Routes for all the controllers and server calls for doing CRUD operations
 route.post("/create", create);
-
+route.get("/getAll", getAll);
+route.get("/getOne/:id", getOne);
+route.put("/update/:id", update);
+route.delete("/deleteUser/:id", deleteUser);
 export default route;
