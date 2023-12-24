464529a157a39236da3c993813fb93de2d1fab0c
ayush-verma018 <vayush1806@gmail.com>
2023-12-24 21:06:27 +0530

    routes created for edit,add,update pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import User from "./components/getUser/User";
+import Edit from "./components/updateUser/Edit";
+import axios from "axios";
 
+//default api url of the api
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+
+//routes defined to move on different pages of the site
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<User />}></Route>
+        <Route path="/add" element={"Add User"}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
